bb4b7150ce27675e090ca767773c53736513b764
Dale Emery <dale@dhemery.com>
2017-11-14 14:23:33 -0800

    Reduced remote control encoder sensitivity

--- src/main/java/com/dhemery/bitwig/PluginEncoders.java
+++ src/main/java/com/dhemery/bitwig/PluginEncoders.java
@@ -10,15 +10,13 @@ import com.dhemery.midi.ControlChangeDispatcher;
 import java.util.List;
 
 public class PluginEncoders {
-    private static final int VOLUME_FADER_RESOLUTION = 128;
-    private static final int PAN_ENCODER_RESOLUTION = 201; // Range from -100% to 100% in 1% increments
-
+    private static final int REMOTE_CONTROL_ENCODER_RESOLUTION = 101;
 
     public PluginEncoders(ControllerHost host, Impulse impulse, ControlChangeDispatcher dispatcher) {
         List<RotaryEncoder> encoders = impulse.mixerEncoders();
 
         CursorTrack cursorTrack = host.createCursorTrack(2, 0);
-        PinnableCursorDevice cursorDevice = cursorTrack.createCursorDevice();
+        CursorDevice cursorDevice = cursorTrack.createCursorDevice();
         CursorRemoteControlsPage cursorRemoteControlsPage = cursorDevice.createCursorRemoteControlsPage(encoders.size());
 
         for (int i = 0; i < encoders.size(); i++) {
@@ -30,6 +28,6 @@ public class PluginEncoders {
         remoteControl.name().markInterested();
         remoteControl.markInterested();
         remoteControl.setIndication(true);
-        dispatcher.register(encoder, v -> remoteControl.inc(encoder.directionOf(v), 201));
+        dispatcher.register(encoder, v -> remoteControl.inc(encoder.directionOf(v), REMOTE_CONTROL_ENCODER_RESOLUTION));
     }
 }
